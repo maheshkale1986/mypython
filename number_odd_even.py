d255e36cecd50f778e9f0ca6500be9a2034adba8
Mahesh Kale <mahesh.kale14@gmail.com>
2023-12-16 10:07:00 +0530

    Create number_odd_even.py

---
+++ number_odd_even.py
@@ -0,0 +1,10 @@
+# Check if a number is even or odd
+
+# Input: Get a number from the user
+number = int(input("Enter a number: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"{number} is an even number.")
+else:
+    print(f"{number} is an odd number.")
